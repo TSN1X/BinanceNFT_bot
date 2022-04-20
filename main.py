d6b8c7b35bc8edf62f0b68f5c94686be5948df08
Myrzabekov Adilet <myrzabekovadik7@gmail.com>
2022-04-20 14:37:36 +0600

    Comments

--- main.py
+++ main.py
@@ -17,10 +17,15 @@ firefox = 'geckodriver.exe'
 link_b = 'https://www.binance.com/ru/nft/goods/detail?productId='
 link_a = '&isProduct=1'
 
-#assets = [45161845, 45161843]
-#47129861 test
-assets = [46144761]
-search = False
+
+#Это у нас страницы на которых можно конкретно кроссы степн купить . Их две
+assets = [45161845, 45161843]
+
+#Эту юзал для тестов
+#assets = [46144761]
+
+
+
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--disable-extensions')
 chrome_options.add_argument('--profile-directory=Default')
@@ -31,9 +36,18 @@ chrome_options.add_argument("--start-maximized")
 pressed = False
 
 def runSearching(asset):
+
+			#ЗДЕСЬ ВЫБРАТЬ БРАУЗЕР
 			driver = webdriver.Chrome(service=Service(yandex),options = chrome_options)
+			#driver = webdriver.Firefox(service=Service(firefox),options = chrome_options)
+			#driver = webdriver.Chrome(service=Service(chrome),options = chrome_options)
+
+
+
 			link = link_b + str(asset) + link_a
 			driver.get(link)
+			#Timeout - время ожидания пока ты нажмешь кнопку когда страница прогрузилась . Кнопка "0" на нампаде (Должен быть включен) или цифры
+			#Если кнопка "0" не удобна можно заменить на "space" - можно и на другие , но надо найти синтаксис
 			wait(lambda :keyboard.read_key() == '0',timeout_seconds = 1200000 )
 
 			el = WebDriverWait(driver, 100000).until(EC.presence_of_element_located((By.CLASS_NAME, 'css-1gv20g8')))
@@ -41,6 +55,7 @@ def runSearching(asset):
 			el2 = WebDriverWait(driver, 100000).until(EC.presence_of_element_located((By.CLASS_NAME, 'css-1p9f32f')))
 			driver.find_element(By.CLASS_NAME, 'css-1p9f32f').click()
 
+			#Если меняешь кнопку меняй и здесь тоже
 			wait(lambda :keyboard.read_key() == '0',timeout_seconds = 1200000 )
 			#css-19v461i test
 			#css-1p9f32f real
